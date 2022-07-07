13c8e25914cd5e9645ac53b11b31d424fe23846c
ZainAmjad68 <zainamjad68@yahoo.com>
2022-07-07 17:51:40 +0500

    Updates

---
+++ precache-manifest.e0f6c939cd86f3972d35948aa0d1f6e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b136031ecf1cdb784615178fd86a06d4",
+    "url": "/tailwind-dashboard/index.html"
+  },
+  {
+    "revision": "cafe15cc27c4c6cc7f40",
+    "url": "/tailwind-dashboard/static/css/main.340a3a44.chunk.css"
+  },
+  {
+    "revision": "b8ef02a70bdccf2c3175",
+    "url": "/tailwind-dashboard/static/js/2.c04ed867.chunk.js"
+  },
+  {
+    "revision": "cafe15cc27c4c6cc7f40",
+    "url": "/tailwind-dashboard/static/js/main.da143d5d.chunk.js"
+  },
+  {
+    "revision": "326c898c29680ff552f2",
+    "url": "/tailwind-dashboard/static/js/runtime~main.9e951e77.js"
+  }
+]);
